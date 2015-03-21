2851b87d397e784238a04d92f5ca3e36654be990
dhoman <dhoman@huskers.unl.edu>
2015-03-20 20:48:06 -0500

    Yay backgrounds

--- www/js/app.js
+++ www/js/app.js
@@ -27,14 +27,10 @@ angular.module('starter', ['ionic', 'ionic.contrib.ui.tinderCards', 'starter.ser
 
 .controller('CardsCtrl', function($scope, TDCardDelegate, giphyService) {
   console.log('CARDS CTRL');
-  // var cardTypes = [
-  //   { image: 'https://pbs.twimg.com/profile_images/546942133496995840/k7JAxvgq.jpeg' },
-  //   { image: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png' },
-  //   { image: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg' },
-  // ];
   var cardTypes = [];
-  var _this = this;
-  giphyService.trending(0, function(resp){
+  var refreshes = 0;
+
+  giphyService.trending(refreshes, function(resp){
     console.log(resp);
     angular.forEach(resp.data, function(giphy){
       //console.log(giphy.images.fixed_width.url + ' '+giphy.id);
@@ -42,35 +38,35 @@ angular.module('starter', ['ionic', 'ionic.contrib.ui.tinderCards', 'starter.ser
     })
     $scope.cards = cardTypes;
   });
-  // giphyService.getGifs().then(function(resp){
-  //   console.log(resp);
-  //   angular.forEach(resp.data.data, function(giphy){
-  //     //console.log(giphy.images.fixed_width.url + ' '+giphy.id);
-  //     cardTypes.push({ image: giphy.images.fixed_width.url, id: giphy.id})
-  //   })
-  //   $scope.cards = cardTypes;
-  // })
-
- // $scope.cards = Array.prototype.slice.call(cardTypes, 0);
 
   $scope.cardDestroyed = function(index) {
     $scope.cards.splice(index, 1);
   };
 
-  $scope.addCard = function() {
-    var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
-    newCard.id = Math.random();
-    $scope.cards.push(angular.extend({}, newCard));
+  $scope.addCards = function() {
+    refreshes++;
+    giphyService.trending(refreshes, function(resp){
+      console.log(resp);
+      angular.forEach(resp.data, function(giphy){
+        //console.log(giphy.images.fixed_width.url + ' '+giphy.id);
+        cardTypes.push({ image: giphy.images.fixed_width.url, id: giphy.id})
+      })
+      $scope.cards = cardTypes;
+    });
   }
 })
 
 .controller('CardCtrl', function($scope, TDCardDelegate) {
   $scope.cardSwipedLeft = function(index) {
     console.log('LEFT SWIPE');
-    $scope.addCard();
+    if($scope.cards.length === 0){
+      $scope.addCards();
+    }
   };
   $scope.cardSwipedRight = function(index) {
     console.log('RIGHT SWIPE');
-    $scope.addCard();
+    if($scope.cards.length === 0){
+      $scope.addCards();
+    }
   };
 });
